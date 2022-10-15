6241ef081797ffe0206b0afa845872c9e28749d7
Raghu K <raghuk.vit@gmail.com>
2022-10-15 09:10:25 +0530

    Update terraform-modules/terraform-immutable-app/

--- asg.tf
+++ asg.tf
@@ -37,7 +37,8 @@ resource "aws_launch_template" "launch-template" {
   }))
 }
 
-resource "aws_autoscaling_group" "bar" {
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.COMPONENT}-${var.ENV}"
   vpc_zone_identifier = data.terraform_remote_state.infra.outputs.app_subnets
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
