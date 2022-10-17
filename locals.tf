3d04ae5bbd639b9f65f810883c1a25d2d284e126
Raghu K <raghuk.vit@gmail.com>
2022-10-17 11:17:12 +0530

    Update terraform-modules/terraform-immutable-app/

--- locals.tf
+++ locals.tf
@@ -5,6 +5,7 @@ locals {
   username = element(split("/", data.aws_ssm_parameter.credentials.value), 0)
   password = element(split("/", data.aws_ssm_parameter.credentials.value), 1)
 
-  arn = var.COMPONENT == "frontend" ? data.terraform_remote_state.infra.outputs.private_lb_listener_arn : aws_lb_target_group.tg.*.arn[0]
+  arn = aws_lb_target_group.tg.*.arn[0]
+  //arn = var.COMPONENT == "frontend" ? data.terraform_remote_state.infra.outputs.private_lb_listener_arn : aws_lb_target_group.tg.*.arn[0]
 
 }
