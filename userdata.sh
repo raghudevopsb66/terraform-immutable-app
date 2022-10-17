f080026406767c9557c9ffcb18588c690f7e56ff
Raghu K <raghuk.vit@gmail.com>
2022-10-17 08:02:54 +0530

    Update terraform-modules/terraform-immutable-app/

--- userdata.sh
+++ userdata.sh
@@ -6,8 +6,13 @@ if [ -f /etc/nginx/default.d/roboshop.conf ]; then
   systemctl restart filebeat
   exit
 fi
+
+cat /etc/systemd/system/${COMPONENT}.service >>/opt/debug
+echo '----' >>/opt/debug
+echo "Environment=REDIS_HOST=${REDIS_ENDPOINT}" >>/opt/debug
+
 MEM=$(echo $(free -m  | grep ^Mem | awk '{print $2}')*0.8 |bc | awk -F . '{print $1}')
-sed -i -e "s/ENV/${ENV}/" -e "/REDIS_HOST/ c Environment=REDIS_HOST=${REDIS_ENDPOINT}" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e "s/DOCDB_PASS/${DOCDB_PASS}/" /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
+sed -i -e "s/ENV/${ENV}/" -e "/Environment=REDIS_HOST=/ c Environment=REDIS_HOST=${REDIS_ENDPOINT}" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e "s/DOCDB_PASS/${DOCDB_PASS}/" /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 
 systemctl daemon-reload
 systemctl restart ${COMPONENT}
