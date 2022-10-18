ee7ef33fbf1d9a84bb18edf85ef9ce0133ad72a6
Raghu K <raghuk.vit@gmail.com>
2022-10-18 08:14:32 +0530

    Update terraform-modules/terraform-immutable-app/

--- userdata.sh
+++ userdata.sh
@@ -14,7 +14,7 @@ sed -i  -e "s/ENV/${ENV}/" \
         -e "/Environment=REDIS_HOST=/ c Environment=REDIS_HOST=${REDIS_ENDPOINT}" \
         -e "/MONGO_URL/ c Environment=MONGO_URL=\"mongodb://${DOCDB_USER}:${DOCDB_PASS}@${DOCDB_ENDPOINT}:27017/${DB_NAME}?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false\"" \
         -e "/DB_HOST/ c Environment=DB_HOST=${MYSQL_ENDPOINT}" \
-        -e "s/1439/${MEM}/" \
+        -e "s/1439/$MEM/g" \
          /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 
 systemctl daemon-reload
